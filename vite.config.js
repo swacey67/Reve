e9fc3c153f72464186f6508baad27447290324c8
Fawwaz <fawwazsajid@gmail.com>
2026-04-04 15:15:04 +0700

    fix: update base path and sync description changes

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react' // Ubah baris ini
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  // HANYA nama repository GitHub kamu. Jangan masukkan nama folder lokal.
   base: '/Reve/', 
 })
