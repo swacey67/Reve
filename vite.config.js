c05880f0abba7c45ee2161a2d4383016548d3705
Fawwaz <fawwazsajid@gmail.com>
2026-04-10 22:38:51 +0700

    fix: remove base path completely for vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // HANYA nama repository GitHub kamu. Jangan masukkan nama folder lokal.
-  base: '/Reve/', 
+  // Pastikan baris "base: '/Reve/'," SUDAH DIHAPUS
 })
